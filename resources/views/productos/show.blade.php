92c51065e295986cd8ce9cd9e271df02df7caf46
Jose Ramon Lopez <alumno@vm1>
2020-12-04 10:04:01 +0100

    Solucion total a la tarea 3

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -5,18 +5,29 @@
 
         <div class="col-sm-4">
 
-            <a href="{{ url('/productos/show/' . $id ) }}">
+            <a href="{{ url('/productos/show/' . $producto->id ) }}">
                 <img src="https://picsum.photos/200/300/?random" style="height:200px"/>
             </a>
 
         </div>
         <div class="col-sm-8">
 
-            <h4>{{$producto[0]}}</h4><!-- Posicion cero del array $producto, contiene el nombre del producto -->
-            <h6>Producto: {{$producto[1]}}</h6>
-            <p><strong>Estado:</strong> Producto actualmente comprado.</p>
-            <button type="button" class="btn btn-danger" id="pendienteCompra" name="pendienteCompra" value="1">Pendiente de compra</button>
-            <a class="btn btn-warning" href="{{ url('/productos/edit/' . $id ) }}">
+            <h4>{{$producto->nombre}}</h4>
+            <h6>Producto: {{$producto->categoria}}</h6>
+            <p><strong>Estado:</strong>
+                @if($producto->pendiente)
+                    Producto en stock.
+                @else
+                    Producto actualmente comprado.
+                @endif
+            </p>
+
+            @if($producto->pendiente)
+                <a class="btn btn-danger" href="#">Pendiente de compra</a>
+            @else
+                <a class="btn btn-primary" href="#">Producto comprado</a>
+            @endif
+            <a class="btn btn-warning" href="{{ url('/productos/edit/' . $producto->id ) }}">
                 <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
                 Editar producto</a>
             <a class="btn btn-outline-info" href="{{ action('App\Http\Controllers\ProductoController@getIndex') }}">Volver al listado</a>
